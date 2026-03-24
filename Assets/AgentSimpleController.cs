d13d204c4e62319ee7f5aeecfa062b833c095137
ChemsLGDesu <banitessebastian2decundaria@gmail.com>
2026-03-24 11:07:48 -0500

    Update AgentSimpleController.cs

--- Assets/AgentSimpleController.cs
+++ Assets/AgentSimpleController.cs
@@ -18,4 +18,22 @@ public class AgentSimpleController : MonoBehaviour
             agent.SetDestination(Target.position);
         }
     }
+    public void HasPath()
+    {
+        print(agent.hasPath);
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        if (agent == null || agent.path == null) return;
+
+        Vector3[] corners = agent.path.corners;
+
+
+        for (int i = 0; i < corners.Length - 1; i++)
+        {
+            Gizmos.DrawLine(corners[i], corners[i + 1]);
+            Gizmos.DrawSphere(corners[i], 0.2f);
+        }
+    }
 }
